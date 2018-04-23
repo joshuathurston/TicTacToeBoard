a5b84e7de9e0858b0232a8593d3dc6ebc662c8f6
joshuathurston <kbuffardi@csuchico.edu>
2018-04-23 07:41:55 +0000

    implemented TicTacToe functions and unit testing

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,11 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  if(turn == X)
+    turn = O;
+  else
+    turn = X;
+  return turn;
 }
 
 /**
@@ -33,7 +37,17 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+  if(row > 2 || row < 0 || column > 2 || column < 0)
+    return Invalid;
+  if(board[row][column] == Blank)
+  {
+    board[row][column] = turn;
+    toggleTurn();
+    return board[row][column];
+  }
+  else
+    return board[row][column];
+  
 }
 
 /**
@@ -42,7 +56,10 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+  if(row > 2 || row < 0 || column > 2 || column < 0){
+    return Invalid;
+  }
+  return board[row][column];
 }
 
 /**
@@ -51,5 +68,35 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  for(int i = 0; i < BOARDSIZE; i++)
+  {
+    if(board[i][0] == X && board [i][1] == X && board[i][2] == X)
+      return X;
+    if(board[i][0] == O && board [i][1] == O && board[i][2] == O)
+      return O;
+  }
+  for(int i = 0; i < BOARDSIZE; i++)
+  {
+    if(board[0][i] == X && board [1][i] == X && board[2][i] == X)
+      return X;
+    if(board[0][i] == O && board [1][i] == O && board[2][i] == O)
+      return O;
+  }
+  if(board[0][0] == X && board[1][1] == X && board[2][2] == X)
+    return X;
+  if(board[0][0] == O && board[1][1] == O && board[2][2] == O)
+    return O;
+  if(board[0][2] == X && board[1][1] == X && board[2][0] == X)
+    return X;
+  if(board[0][2] == O && board[1][1] == O && board[2][0] == O)
+    return O;
+  for(int i = 0; i < BOARDSIZE; i++)
+  {
+    for(int j = 0; j < BOARDSIZE; j++)
+    {
+      if(board[i][j] == Blank)
+        return Invalid;
+    }
+  }
+  return Blank;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,9 +14,94 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-/* EXAMPLE TEST FORMAT
+// EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
-*/
+
+TEST(TicTacToeBoardTest, turn_was_x)
+{
+	TicTacToeBoard obj;
+	ASSERT_TRUE(obj.toggleTurn() == O);
+}
+
+TEST(TicTacToeBoardTest, turn_toggle_second)
+{
+	TicTacToeBoard obj;
+    obj.toggleTurn();
+	ASSERT_EQ(obj.toggleTurn(), X);
+}
+
+TEST(TicTacToeBoardTest, set_piece_0_0_x)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.placePiece(0, 0), X);
+}
+
+TEST(TicTacToeBoardTest, out_of_bounds_greater)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.placePiece(3,3), Invalid);
+}
+
+TEST(TicTacToeBoardTest, out_of_bound_lower)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.placePiece(-1, -1), Invalid);
+}
+
+TEST(TicTacToeBoardTest, same_position)
+{
+	TicTacToeBoard obj;
+	obj.placePiece(0,0);
+	ASSERT_EQ(obj.placePiece(0,0), X);
+}
+
+TEST(TicTacToeBoardTest, get_blank)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.getPiece(1,1), Blank);
+}
+
+TEST(TicTacToeBoardTest, invalid_get)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.getPiece(-1,3), Invalid);
+}
+
+TEST(TicTacToeBoardTest, get_placed_value)
+{
+    TicTacToeBoard obj;
+    obj.placePiece(0,0);
+    ASSERT_EQ(obj.getPiece(0,0), X);
+}
+
+TEST(TicTacToeBoardTest, check_if_win_X)
+{
+	TicTacToeBoard obj;
+	obj.placePiece(0,0);
+	obj.placePiece(1,0);
+	obj.placePiece(0,1);
+	obj.placePiece(2,0);
+	obj.placePiece(0,2);
+	ASSERT_EQ(obj.getWinner(), X);
+}
+
+TEST(TicTacToeBoardTest, check_if_win_O)
+{
+	TicTacToeBoard obj;
+	obj.placePiece(1,1);
+	obj.placePiece(1,0);
+	obj.placePiece(0,1);
+	obj.placePiece(2,0);
+	obj.placePiece(2,2);
+	obj.placePiece(0,0);
+	ASSERT_EQ(obj.getWinner(), O);
+}
+
+TEST(TicTacToeBoardTest, check_invalid_win)
+{
+	TicTacToeBoard obj;
+	ASSERT_EQ(obj.getWinner(), Invalid);
+}
